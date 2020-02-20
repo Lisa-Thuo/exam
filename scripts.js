d827bd8dabf07374101af5212eade3eed829184f
Lisa-Thuo <jebetthuo@gmail.com>
2020-02-20 22:55:02 +0300

    everything

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function increment() {
+    var string = prompt("Write Something")
+    var lastElement = string.charAt(string.length-1);
+    alert (string+lastElement.length);
+    if (lastElement == true) {
+      return string.indexOf++;
+    }
+  }
+  
+  increment();
